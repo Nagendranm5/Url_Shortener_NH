53667db981b58922087f8763033444581aaab483
Nagendranm5 <nagendransri3@gmail.com>
2022-02-07 12:16:06 +0530

    URL Get Method Modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :urls
   get 'info/:generated_url', to: 'urls#show', as: 'myurl'
-  get '/io/:generated_url', to: 'urls#get_original_url'
+  get '/:generated_url', to: 'urls#get_original_url'
   root 'urls#index'
 end
