ee200f044202a7579b4edf7cd50ce279fe49618f
Nagendranm5 <nagendransri3@gmail.com>
2022-02-15 23:13:03 +0530

    QR Code Generated

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -31,8 +31,40 @@ class UrlsController < ApplicationController
 	end
 
 	def generate_qr_code
-		# Url.generate_qr_code2(params[:generated_url])
+		@url = Url.find_by_id(params[:generated_url])
+		@original_url = @url.original_url
+		qrcode = RQRCode::QRCode.new(@original_url)
+		png = qrcode.as_png(
+		  bit_depth: 1,
+		  border_modules: 4,
+		  color_mode: ChunkyPNG::COLOR_GRAYSCALE,
+		  color: "black",
+		  file: nil,
+		  fill: "white",
+		  module_px_size: 6,
+		  resize_exactly_to: false,
+		  resize_gte_to: false,
+		  size: 150
+		)
+
+		IO.binwrite("tmp/#{@url.generated_url}.png", png.to_s)
+
+		blob = ActiveStorage::Blob.create_and_upload!(
+			io: File.open("tmp/#{@url.generated_url}.png"),
+			filename: (@url.generated_url),
+			content_type: 'png',
+		)
+
+		@url.qrcodepng.attach(blob)
+		@url.update(qrcode: true)
+		redirect_back fallback_location: root_path
 	end
+
+	# def download_qr_code
+	# 	@url = Url.find_by_id(params[:generated_url])
+	# 	send_file "/home/ubuntu/Projects/url_shortener/tmp/7f4a09c0.png", :type => 'application/png', :x_sendfile => true
+	# 	redirect_back fallback_location: root_path
+	# end
 
 	private
 	def url_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'info/:generated_url', to: 'urls#show', as: 'myurl'
   get '/:generated_url', to: 'urls#get_original_url'
   get '/generate_qr_code/:generated_url', to: 'urls#generate_qr_code', as: 'generate_qr_code'
+  # get '/download_qr_code/:generate_url', to: 'urls#download_qr_code', as: 'download_qr_code'
   root 'urls#index'
 end
